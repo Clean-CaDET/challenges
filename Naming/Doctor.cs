63a48d40210576446f2d44c0375df8b881a61cf4
Nikola Luburić <luburic91@gmail.com>
2024-01-11 11:50:51 +0100

    Update Doctor.cs

--- Naming/Doctor.cs
+++ Naming/Doctor.cs
@@ -2,93 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Naming
+namespace Naming;
+
+/// <summary>
+/// Unit: Meaningful names; KC: Remove noise words
+/// </summary>
+public class DoctorInfo
 {
-    /// <summary>
-    /// ID izazova je dostupan na web prikazu.
-    /// ** Nemoj da modifikuješ naziv namespace-a, Run klase i zaglavlja metoda ove klase. **
-    /// 1. Odredi sve nazive koji sadrže generične ili beznačajne reči.
-    /// 2. Ukloni ove reči ili ih promeni u nešto značajnije.
-    /// 3. Potvrdi da nema sintaksnih grešaka i da je Run klasa ispratila bilo kakva preimenovanja klasa i metoda koje koristi.
-    /// </summary>
-    public class DoctorInfo
+  public int Id {get;}
+  public ISet<Certificate> Certificates {get;}
+
+  public bool HasCertificates(
+    List<Certificate> certificateSet)
+  {
+    foreach (var c in certificateSet)
     {
-        public int DoctorId { get; }
-        public ISet<Specialization> Specializations { get; } = new HashSet<Specialization>();
+      if (!Certificates.Contains(c))
+        return false;
+    }
+    return true;
+  }
+}
 
-        public bool HasSpecializations(List<Specialization> specializationSet)
-        {
-            foreach (var specialization in specializationSet)
-            {
-                if (!Specializations.Contains(specialization)) return false;
-            }
+public class Certificate
+{
+    public string Name;
 
-            return true;
-        }
-
-        public void AssignSpecialization(Specialization specializationData)
-        {
-            if (Specializations.Contains(specializationData)) throw new TheDoctorAlreadyHasSpecializationException("Doctor: " + DoctorId.ToString() + "Spec: " + specializationData);
-            Specializations.Add(specializationData);
-        }
-
+    public Specialization(string name)
+    {
+        Name = name;
     }
 
-    public class TheDoctorAlreadyHasSpecializationException : Exception
+    public override bool Equals(object obj)
     {
-        public TheDoctorAlreadyHasSpecializationException(string doctorId) : base(doctorId)
-        {
-        }
+        if (!(obj is Certificate other)) return false;
+        return other.Name.Equals(Name);
     }
 
-    public class Specialization
+    public override int GetHashCode()
     {
-        private readonly string _nameStr;
-
-        public Specialization(string name)
-        {
-            _nameStr = name;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is Specialization other)) return false;
-            return other._nameStr.Equals(_nameStr);
-        }
-
-        public override int GetHashCode()
-        {
-            return _nameStr.GetHashCode();
-        }
+        return Name.GetHashCode();
     }
-
-    #region Run
-    public class Run
-    {
-        private readonly DoctorInfo _doctor;
-
-        public Run()
-        {
-            _doctor = new DoctorInfo();
-            _doctor.AssignSpecialization(new Specialization("Test 1"));
-            _doctor.AssignSpecialization(new Specialization("Test 2"));
-            _doctor.AssignSpecialization(new Specialization("Test 3"));
-        }
-
-        public void AddSpec()
-        {
-            _doctor.AssignSpecialization(new Specialization("Test 1"));
-        }
-
-        public List<Specialization> GetSpec()
-        {
-            return _doctor.Specializations.ToList();
-        }
-
-        public bool HasSpec(List<Specialization> all)
-        {
-            return _doctor.HasSpecializations(all);
-        }
-    }
-    #endregion
 }
